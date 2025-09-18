aa21dc54175d7476f2384898a7e7f4b0c865052e
Shwet Prakash <krish.shwetprakash08@gmail.com>
2025-09-18 23:28:22 +0530

    Sprites Loaded and Draw

--- tutorial.py
+++ tutorial.py
@@ -20,10 +20,81 @@ window = pygame.display.set_mode((WIDTH, HEIGHT))
 
 
 
+
+
+# ---------------------------- Helper functions ----------------------------
+
+# Load background as required
+def get_background(color_name):
+    image = pygame.image.load(join("assets", "Background", color_name))
+    _, _, width, height = image.get_rect()
+
+    tiles = []
+    # marking locations (coords) for image/tile placement
+    for i in range(WIDTH //width + 1):
+        for j in range(HEIGHT // height + 1):
+            pos = (i*width, j*height)
+            tiles.append(pos)
+        
+    return tiles, image
+
+# Flip sprites when bi-directional required
+def flip(sprites):
+    # flip a sprite x-axis (horizontally)
+    return [pygame.transform.flip(sprite, True, False) for sprite in sprites]
+
+# Load any kind of sprite
+def load_sprite_sheets(dir1, dir2, width, height, direction=False):
+    path = join("assets", dir1, dir2)
+    images = [file for file in listdir(path) if isfile(join(path, file))]
+
+    all_sprites = {}
+
+    for image in images:
+        sprite_sheet = pygame.image.load(join(path, image)).convert_alpha()
+
+        sprites = []
+        for i in range(sprite_sheet.get_width() // width):
+            surface = pygame.Surface((width, height), pygame.SRCALPHA, 32)
+            rect = pygame.Rect(i*width, 0, width, height)
+            surface.blit(sprite_sheet, (0,0), rect)
+
+            sprites.append(pygame.transform.scale2x(surface))
+        
+        if direction:
+            all_sprites[image.replace(".png", "") + "_right"] = sprites
+            all_sprites[image.replace(".png", "") + "_left"] = flip(sprites)
+        else:
+            all_sprites[image.replace(".png", "") + ""] = sprites
+    
+    return all_sprites
+
+
+# Draw on window (Screen)
+def draw(window, background, bg_image, player):
+    for tile in background:
+        window.blit(bg_image, tile)
+
+    player.draw(window)
+    pygame.display.update() # Clear and repaint
+
+# Handle Movement of Players
+def handle_move(player):
+    keys = pygame.key.get_pressed()
+
+    player.x_vel = 0
+    if keys[pygame.K_LEFT]:
+        player.move_left(PLAYER_VEL)
+    if keys[pygame.K_RIGHT]:
+        player.move_right(PLAYER_VEL)
+
+# ---------------------------- Classes --------------------------
+
 # Player Class
 class Player(pygame.sprite.Sprite):
     COLOR = (255, 0, 0) # Red player block
     GRAVITY = 1 # 9.8
+    SPRITES = load_sprite_sheets("MainCharacters", "MaskDude", 32, 32, True)
 
     def __init__(self, x, y, width, height):
         self.rect = pygame.Rect(x, y, width, height)
@@ -55,49 +126,16 @@ class Player(pygame.sprite.Sprite):
 
 
     def loop(self, fps):
-        # How this gravity logic works
-        self.y_vel += min(1, (self.fall_time / fps * self.GRAVITY))
+        # How this gravity logic works : Platform needed
+        # self.y_vel += min(1, (self.fall_time / fps * self.GRAVITY))
         self.move(self.x_vel, self.y_vel)
 
         self.fall_time += 1
 
     def draw(self, window):
-        pygame.draw.rect(window, self.COLOR, self.rect)
-
-
-# Helper functions
-def get_background(color_name):
-    image = pygame.image.load(join("assets", "Background", color_name))
-    _, _, width, height = image.get_rect()
-
-    tiles = []
-    # marking locations (coords) for image/tile placement
-    for i in range(WIDTH //width + 1):
-        for j in range(HEIGHT // height + 1):
-            pos = (i*width, j*height)
-            tiles.append(pos)
-        
-    return tiles, image
-
-
-# Draw on window (Screen)
-def draw(window, background, bg_image, player):
-    for tile in background:
-        window.blit(bg_image, tile)
-
-    player.draw(window)
-    pygame.display.update() # Clear and repaint
-
-# Handle Movement of Players
-def handle_move(player):
-    keys = pygame.key.get_pressed()
-
-    player.x_vel = 0
-    if keys[pygame.K_LEFT]:
-        player.move_left(PLAYER_VEL)
-    if keys[pygame.K_RIGHT]:
-        player.move_right(PLAYER_VEL)
-
+        # pygame.draw.rect(window, self.COLOR, self.rect)
+        self.sprite = self.SPRITES["idle_" + self.direction][0]
+        window.blit(self.sprite, (self.rect.x, self.rect.y))
 
 
 # GAME MASTER CONSOLE
